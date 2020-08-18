5b8d48d1439a22c50e00f759a59f95e096714d20
Berzotti <eduardoberzotti@hotmail.com>
2020-08-18 20:18:07 -0300

    Criando cards

--- src/containers/Main/index.jsx
+++ src/containers/Main/index.jsx
@@ -1,11 +1,28 @@
-import React, { memo } from 'react' // O memo cria um componente imutavel (Evita redenrização desnecessária)
+import React, { memo, useCallback, useState,useEffect } from 'react' // O memo cria um componente imutavel (Evita redenrização desnecessária)
+import Api from '../../api'
+import Board from './components/Board'
+import { ContainerStyled } from './style'
 
 function Main() {
+    const [data, setData] = useState({}) // Usei use state para setar os valores
+    const [country, setCountry] = useState('brazil')
+
+    const getCovidData = useCallback((country) => {
+        Api.getCountry(country) // A api está fazendo a chamada de um país
+            .then(data => setData(data))
+    }, [])
+
+    useEffect(() => { // UseEffect vai ser inicado assim que a página for renderizada
+        getCovidData(country)
+    }, [getCovidData, country])
 
     return (
-        <div>
-            Teste
-        </div>
+        <ContainerStyled>
+            <div className="mb-2">
+
+            </div>
+            <Board data={data} />
+        </ContainerStyled>
     )
 }
 
